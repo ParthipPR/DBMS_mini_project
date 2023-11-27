297718c58582f0b202da84e472e5b24c305aa707
ParthipPR <parthip2512003@gmail.com>
2023-11-27 14:51:28 +0530

    feat: Finished Data_entry page

--- project-root/public/Data_entry.js
+++ project-root/public/Data_entry.js
@@ -8,6 +8,7 @@ $(document).ready(function () {
   fetchRoutes(); 
   fetchSchedules();
   fetchTrainStaff();
+  
 
 
   // Stations table Functions
@@ -44,7 +45,7 @@ $(document).ready(function () {
   }
 
   // Function to delete a station
-  function deleteStation(stationId) {
+function deleteStation(stationId) {
   if (confirm('Are you sure you want to delete this station?')) {
     $.ajax({
       url: `/stations/${stationId}`,
@@ -58,7 +59,7 @@ $(document).ready(function () {
       },
     });
   }
-  }
+}
 
   function updateStation(stationId) {
     // Create a form with input fields for updated information
@@ -274,8 +275,6 @@ function populateTrainsTable(trains) {
 }
 
 // Function for route table
-
-// Add route form submission
 $('#addRouteForm').submit(function (e) {
   e.preventDefault();
   addRoute();
@@ -438,3 +437,305 @@ function populateRoutesTable(routes) {
     });
   });
 }
+
+// Function for Schedules table
+
+// Submit form for adding a new schedule
+$('#addScheduleForm').submit(function (e) {
+  e.preventDefault();
+  addSchedule();
+});
+
+// Function to delete a schedule
+function deleteSchedule(scheduleId) {
+  if (confirm('Are you sure you want to delete this schedule?')) {
+    $.ajax({
+      url: `/schedules/${scheduleId}`,
+      method: 'DELETE',
+      success: function () {
+        fetchSchedules(); // Refresh the schedules table after deleting a schedule
+      },
+      error: function (error) {
+        console.error('Error deleting schedule:', error);
+        alert('Error deleting schedule');
+      },
+    });
+  }
+}
+
+// Function to update a schedule
+function updateSchedule(scheduleId) {
+  // Fetch the train and route names for dropdowns
+  fetchTrainsAndRoutes(function (trains, routes) {
+    // Fetch the schedule details
+    $.get(`/schedules/${scheduleId}`, function (schedule) {
+      // Create a form with input fields for updated information
+      const form = $('<form></form>');
+      form.append(createLabelAndDropdownForScheduleTrain('Updated Train:', 'updatedTrain', trains, schedule.TrainID));
+      form.append(createLabelAndDropdownForScheduleRoute('Updated Route:', 'updatedRoute', routes, schedule.RouteID));
+      form.append('<label for="updatedDepartureTime">Updated Departure Time:</label>');
+      form.append('<input type="datetime-local" id="updatedDepartureTime" name="updatedDepartureTime" required>');
+
+      form.append('<label for="updatedArrivalTime">Updated Arrival Time:</label>');
+      form.append('<input type="datetime-local" id="updatedArrivalTime" name="updatedArrivalTime" required>');
+
+      // Set values based on the existing schedule
+      $('#updatedDepartureTime').val(schedule.DepartureTime.replace(' ', 'T'));
+      $('#updatedArrivalTime').val(schedule.ArrivalTime.replace(' ', 'T'));
+
+      // Show a dialog with the form
+      showDialog('Update Schedule', form, function () {
+        const updatedData = {
+          TrainID: $('#updatedTrain').val(),
+          RouteID: $('#updatedRoute').val(),
+          DepartureTime: $('#updatedDepartureTime').val(),
+          ArrivalTime: $('#updatedArrivalTime').val(),
+        };
+
+        // Send data to the server
+        updateScheduleOnServer(scheduleId, updatedData);
+      });
+    });
+  });
+}
+
+
+// Function to add a new schedule
+function addSchedule() {
+  // Collect form data
+  const formData = {
+    ScheduleID: $('#ScheduleID').val(),
+    TrainID: $('#TrainID').val(),
+    RouteID: $('#RouteID').val(),
+    DepartureTime: $('#DepartureTime').val(),
+    ArrivalTime: $('#ArrivalTime').val(),
+  };
+
+  // Send data to the server
+  $.post('/schedules', formData, function (data) {
+    alert(data.message);
+    // Clear the form after successful addition
+    $('#addScheduleForm')[0].reset();
+    fetchSchedules(); // Refresh the schedule table after adding a schedule
+  });
+}
+
+// Function to fetch schedules from the server
+function fetchSchedules() {
+  $.get('/schedules', function (data) {
+    populateSchedulesTable(data);
+  });
+}
+
+// Function to populate the schedules table with data
+function populateSchedulesTable(schedules) {
+  // Clear existing schedule table rows
+  $('#schedulesTable tbody').empty();
+
+  // Fetch train and route names from the server
+  fetchTrainsAndRoutes(function (trains, routes) {
+    // Create mappings of train and route IDs to their names
+    const trainNameMap = {};
+    trains.forEach(train => {
+      trainNameMap[train.TrainID] = train.TrainName;
+    });
+
+    const routeNameMap = {};
+    routes.forEach(route => {
+      routeNameMap[route.RouteID] = route.RouteName;
+    });
+
+    // Populate schedule table with schedules
+    schedules.forEach(function (schedule) {
+      const row = `<tr>
+                      <td>${schedule.ScheduleID}</td>
+                      <td>${trainNameMap[schedule.TrainID]}</td>
+                      <td>${routeNameMap[schedule.RouteID]}</td>
+                      <td>${formatDateTime(schedule.DepartureTime)}</td>
+                      <td>${formatDateTime(schedule.ArrivalTime)}</td>
+                      <td>
+                        <button onclick="deleteSchedule(${schedule.ScheduleID})">Delete</button>
+                        <button onclick="updateSchedule(${schedule.ScheduleID})">Update</button>
+                      </td>
+                  </tr>`;
+      $('#schedulesTable tbody').append(row);
+    });
+  });
+}
+
+// Helper function to format datetime in a readable format
+function formatDateTime(dateTimeString) {
+  const options = { year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+  return new Date(dateTimeString).toLocaleDateString(undefined, options);
+}
+
+// Function to fetch trains and routes from the server
+function fetchTrainsAndRoutes(callback) {
+  // Fetch trains
+  $.get('/trains', function (trains) {
+    // Fetch routes
+    $.get('/routes', function (routes) {
+      // Execute the callback with trains and routes
+      callback(trains, routes);
+    });
+  });
+}
+
+function createLabelAndDropdownForScheduleTrain(labelText, elementId, data, selectedValue) {
+  const container = $('<div></div>');
+  container.append(`<label for="${elementId}">${labelText}</label>`);
+  container.append(`<select id="${elementId}" name="${elementId}" required></select>`);
+
+  // Populate dropdown with data
+  populateDropdownForScheduleTrain(elementId, data);
+
+  // Set selected value based on the existing route
+  $(`#${elementId}`).val(selectedValue);
+
+  return container;
+}
+function createLabelAndDropdownForScheduleRoute(labelText, elementId, data, selectedValue) {
+  const container = $('<div></div>');
+  container.append(`<label for="${elementId}">${labelText}</label>`);
+  container.append(`<select id="${elementId}" name="${elementId}" required></select>`);
+
+  // Populate dropdown with data
+  populateDropdownForScheduleRoute(elementId, data);
+
+  // Set selected value based on the existing route
+  $(`#${elementId}`).val(selectedValue);
+
+  return container;
+}
+
+function populateDropdownForScheduleTrain(elementId, data) {
+  const dropdown = $('#' + elementId);
+  dropdown.empty();
+  dropdown.append('<option value="" selected>Select Train</option>');
+  $.each(data, function (key, entry) {
+    dropdown.append($('<option></option>').attr('value', entry.TrainID).text(entry.TrainName));
+  });
+}
+function populateDropdownForScheduleRoute(elementId, data) {
+  const dropdown = $('#' + elementId);
+  dropdown.empty();
+  dropdown.append('<option value="" selected>Select Train</option>');
+  $.each(data, function (key, entry) {
+    dropdown.append($('<option></option>').attr('value', entry.RouteID).text(entry.RouteID));
+  });
+}
+
+
+// Helper function to format schedule option text
+function formatScheduleOption(entry) {
+  // Customize this based on the properties of your schedule data
+  return `${entry.TrainName} - ${entry.RouteName} - ${formatDateTime(entry.DepartureTime)}`;
+}
+
+
+// Add this function to fetch Train Staff data
+function fetchTrainStaff() {
+  // Fetch Train Staff data from the server
+  $.get('/trainstaff', function (data) {
+    populateTrainStaffTable(data);
+  });
+}
+
+// Add this function to populate the Train Staff table with data
+function populateTrainStaffTable(trainStaff) {
+  // Clear existing Train Staff table rows
+  $('#trainStaffTable tbody').empty();
+
+  // Populate Train Staff table with Train Staff data
+  trainStaff.forEach(function (staff) {
+    const row = `<tr>
+                    <td>${staff.StaffID}</td>
+                    <td>${staff.TrainID}</td>
+                    <td>${staff.StaffName}</td>
+                    <td>${staff.Designation}</td>
+                    <!-- Add more columns as needed -->
+                    <td>
+                      <button onclick="deleteTrainStaff(${staff.StaffID})">Delete</button>
+                      <button onclick="updateTrainStaff(${staff.StaffID})">Update</button>
+                    </td>
+                </tr>`;
+    $('#trainStaffTable tbody').append(row);
+  });
+}
+
+// Add this function to delete Train Staff
+function deleteTrainStaff(staffId) {
+  if (confirm('Are you sure you want to delete this Train Staff member?')) {
+    $.ajax({
+      url: `/trainstaff/${staffId}`,
+      method: 'DELETE',
+      success: function () {
+        fetchTrainStaff(); // Refresh the Train Staff table after deleting a staff member
+      },
+      error: function (error) {
+        console.error('Error deleting Train Staff member:', error);
+        alert('Error deleting Train Staff member');
+      },
+    });
+  }
+}
+
+// Add this function to update Train Staff information
+function updateTrainStaff(staffId) {
+  // Create a form with input fields for updated information
+  const form = $('<form></form>');
+  form.append('<label for="updatedStaffName">Updated Staff Name:</label>');
+  form.append('<input type="text" id="updatedStaffName" name="updatedStaffName" required>');
+  form.append('<label for="updatedDesignation">Updated Designation:</label>');
+  form.append('<input type="text" id="updatedDesignation" name="updatedDesignation" required>');
+
+  // Show a dialog with the form
+  const dialog = $('<div></div>').append(form);
+  $('body').append(dialog);
+
+  dialog.dialog({
+    title: 'Update Train Staff',
+    modal: true,
+    buttons: {
+      'Update': function () {
+        // Collect updated information from the form
+        const updatedData = {
+          StaffName: $('#updatedStaffName').val(),
+          Designation: $('#updatedDesignation').val(),
+          // Add more fields as needed
+        };
+
+        // Send data to the server
+        $.ajax({
+          url: `/trainstaff/${staffId}`,
+          method: 'PUT',
+          contentType: 'application/json',
+          data: JSON.stringify(updatedData),
+          success: function (data) {
+            console.log('Train Staff member updated successfully:', data);
+            alert(data.message);
+            fetchTrainStaff(); // Refresh the Train Staff table after updating a staff member
+          },
+          error: function (xhr, status, error) {
+            console.error('Error updating Train Staff member:', error);
+            alert('Error updating Train Staff member. Check the console for details.');
+          },
+        });
+
+        // Close the dialog
+        $(this).dialog('close');
+        dialog.remove();
+      },
+      'Cancel': function () {
+        // Close the dialog without updating
+        $(this).dialog('close');
+        dialog.remove();
+      }
+    },
+    close: function () {
+      // Remove the dialog from the DOM
+      $(this).dialog('destroy').remove();
+    }
+  });
+}
+
